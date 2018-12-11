0991f1ab82f7508413b4640aacde4bb692ed5a3e
Matthew McMillan <matt.mcmillan@silvercar.com>
2018-12-11 15:20:41 -0600

    Add itsybitsy config for SERCOM0

--- boards/itsybitsy_m0_UART_SERCOM3/board_config.h
+++ boards/itsybitsy_m0_UART_SERCOM3/board_config.h
@@ -4,7 +4,7 @@
 #define CRYSTALLESS    1
 
 #define VENDOR_NAME "Adafruit Industries"
-#define PRODUCT_NAME "ItsyBitsy M0 Express UART"
+#define PRODUCT_NAME "ItsyBitsy M0 Express UART SERCOM3"
 #define VOLUME_LABEL "ITSYUART"
 #define INDEX_URL "http://adafru.it/3727"
 #define BOARD_ID "SAMD21G18A-ItsyBitsy-v0"
@@ -23,8 +23,8 @@
 #define BOOT_USART_PAD_SETTINGS           UART_RX_PAD1_TX_PAD0
 #define BOOT_USART_PAD3                   PINMUX_UNUSED
 #define BOOT_USART_PAD2                   PINMUX_UNUSED
-#define BOOT_USART_PAD1                   PINMUX_PA23C_SERCOM3_PAD1
-#define BOOT_USART_PAD0                   PINMUX_PA22C_SERCOM3_PAD0
+#define BOOT_USART_PAD1                   PINMUX_PA23C_SERCOM3_PAD1 //Pin 'SCL' on ItsyBitsy M0 -> HOST RXI
+#define BOOT_USART_PAD0                   PINMUX_PA22C_SERCOM3_PAD0 //Pin 'SDA' on ItsyBitsy M0 -> HOST TXO
 #define BOOT_GCLK_ID_CORE                 SERCOM3_GCLK_ID_CORE
 #define BOOT_GCLK_ID_SLOW                 SERCOM3_GCLK_ID_SLOW
 
